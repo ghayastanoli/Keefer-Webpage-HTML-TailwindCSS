6a3c27fc8a4a5a6a968fd89908b8999a2f393d4c
[ghayastanoli] <[ghiyastanoli@yahoo.com]>
2025-01-04 02:37:57 +0500

    chore: sections added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          'customFont': ["ariblk", "sans"],
+        },
+      },
+    },
+    // Other Tailwind configuration settings
+  };
